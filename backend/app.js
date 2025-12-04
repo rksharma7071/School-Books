eb862bb3fd36c18ab50ba7f1f923b05319871a3d
Retesh Kumar Sharma <rksharma7071@gmail.com>
2025-12-04 12:06:57 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.status(200).send("Hello");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running https://localhost:${PORT}`);
+});
