28fd39392a16b268b1c6b0335bb36569f97a7954
Retesh Kumar Sharma <rksharma7071@gmail.com>
2026-01-15 11:55:12 +0530

    update sitemap

--- frontend/generate-sitemap.js
+++ frontend/generate-sitemap.js
@@ -37,13 +37,15 @@ async function generateSitemap() {
     const dynamicRoutes = await getDynamicRoutes();
     const allRoutes = [...staticRoutes, ...dynamicRoutes];
 
-    const xml = `<?xml version="1.0" encoding="UTF-8"?>
+    const xml = 
+`<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${allRoutes
     .map(
         (route) => `
   <url>
     <loc>${SITE_URL}${route}</loc>
+    <lastmod>${new Date().toISOString().split("T")[0]}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>${route === "/" ? "1.0" : "0.8"}</priority>
   </url>`
